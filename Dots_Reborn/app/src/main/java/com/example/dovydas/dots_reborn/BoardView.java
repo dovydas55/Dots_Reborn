7e05fdc673ec9f53b37636effc9e01b36e0b4236
Dovydas <dovydas55@hotmail.com>
2015-09-10 18:02:14 +0000

    custom layout for in game landscape mode added

--- Dots_Reborn/app/src/main/java/com/example/dovydas/dots_reborn/BoardView.java
+++ Dots_Reborn/app/src/main/java/com/example/dovydas/dots_reborn/BoardView.java
@@ -16,12 +16,22 @@ public class BoardView extends View {
     @Override
     protected void onMeasure( int widthMeasureSpec, int heightMeasureSpec ) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
         int width  = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
         int height = getMeasuredHeight() - getPaddingTop() - getPaddingBottom();
         int size = Math.min(width, height);
         setMeasuredDimension(size + getPaddingLeft() + getPaddingRight(),
                 size + getPaddingTop() + getPaddingBottom());
+
     }
+
+    @Override
+    protected void onSizeChanged( int xNew, int yNew, int xOld, int yOld ) {
+        //setPadding(80,80,80,80);
+
+
+    }
+
 
 
 }
