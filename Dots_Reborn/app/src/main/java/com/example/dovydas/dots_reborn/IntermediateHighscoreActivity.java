31319c9cacb0b3602e5a9a857ec0e6fe68056388
gunnhildurf <gunnhildurf@gmail.com>
2015-09-13 20:05:02 +0000

    Animation on highscore select

--- Dots_Reborn/app/src/main/java/com/example/dovydas/dots_reborn/IntermediateHighscoreActivity.java
+++ Dots_Reborn/app/src/main/java/com/example/dovydas/dots_reborn/IntermediateHighscoreActivity.java
@@ -4,17 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 
 public class IntermediateHighscoreActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.dovydas.dots_reborn.MESSAGE";
+
+    ViewGroup _highScoreSelect;
+    private SquareIcon _selectMoves;
+    private SquareIcon _selectTimed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intermediate_highscore);
+
+        _highScoreSelect = (ViewGroup) findViewById(R.id.highScoreSelect);
+        _selectMoves = (SquareIcon) findViewById(R.id.highScoreMoveMode);
+        _selectTimed = (SquareIcon) findViewById(R.id.highScoreTimeMode);
+
 
         ActionBar action = getSupportActionBar();
         action.setDisplayShowHomeEnabled(true);
@@ -45,6 +59,30 @@ public class IntermediateHighscoreActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+
+            int right = _highScoreSelect.getRight();
+
+
+            TranslateAnimation transMoves = new TranslateAnimation(-1000, 0, 0, 0);
+            transMoves.setDuration(800);
+            transMoves.setFillAfter(true);
+
+            TranslateAnimation transTimed = new TranslateAnimation(right + 200, 0, 0, 0);
+            transTimed.setStartOffset(300);
+            transTimed.setDuration(800);
+            transTimed.setFillAfter(true);
+
+
+            _selectMoves.startAnimation(transMoves);
+            _selectTimed.startAnimation(transTimed);
+
+        }
     }
 
     public void showMoveHighScore(View v){
